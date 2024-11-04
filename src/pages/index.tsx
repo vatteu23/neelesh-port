699ad02deb9626e20cd94a38216471b86388ef3e
Uday Vatti <uvatti@labelbox.com>
2024-11-04 15:11:39 -0600

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,36 +5,77 @@ import Typography from "@/components/Typography";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
-
-
-
   return (
     <Layout>
-
       <HeadWithMetas
         title="Neelesh Reddy | Unreal Engine Generalist"
         description="Neelesh Reddy is an Unreal Engine generalist with a passion for creating immersive experiences."
       />
-    
 
-      <div className="!mt-4 m-4 md:m-12 bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600  animate-gradient-x md:p-6 rounded-xl">
-      <div className="max-w-4xl w-full space-y-8 mx-auto py-12">
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          className="text-center"
-        >
-           <Typography variant="h3" wrapper="h1" className="mb-3" color="light">Hi, I&apos;m Neelesh Reddy &#128075;</Typography>
-              <Typography variant="h4" fontWeight="light" color="light" className="mb-12">
-                An Unreal engine generalist
-              </Typography>
-             
-        </motion.div>
+      <div
+        className="
+      flex items-center
+      !mt-4 m-4 md:m-12 !mb-0 bg-gradient-to-r min-h-[75vh] from-teal-400 via-blue-500 to-indigo-600  animate-gradient-x md:p-6 rounded-xl"
+      >
+        <div className="max-w-4xl w-full space-y-8 mx-auto py-12">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="text-center"
+          >
+            <Typography
+              variant="h3"
+              wrapper="h1"
+              className="mb-3"
+              color="light"
+            >
+              Hi, I&apos;m Neelesh Reddy &#128075;
+            </Typography>
+            <Typography
+              variant="h4"
+              fontWeight="light"
+              color="light"
+              className="mb-12"
+            >
+              An Unreal engine generalist
+            </Typography>
 
+            <div className="flex flex-row flex-wrap items-center justify-center gap-4 mt-6">
+              <a
+                href="#demo"
+                className="block bg-white/70 p-4 rounded-lg text-black backdrop-blur-lg w-full max-w-[200px] text-center transition-transform transform hover:scale-105"
+                onClick={(e) => {
+                  e.preventDefault();
+                  const target = document.getElementById("demo");
+                  if (target) {
+                    target.scrollIntoView({
+                      behavior: "smooth",
+                      block: "start",
+                    });
+                  }
+                }}
+              >
+                View Demo Reel
+              </a>
+
+              <Link
+                href="https://www.linkedin.com/in/neelesh-reddy-botta-3405291a6/"
+                target="_blank"
+                className="block bg-white/10 p-4 rounded-lg text-white backdrop-blur-lg w-full max-w-[200px] text-center transition-transform transform hover:scale-105"
+              >
+                Get In Touch
+              </Link>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      <div className="pt-6 pb-24 px-6 lg:px-12" id="demo">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -50,55 +91,65 @@ export default function Home() {
             ></iframe>
           </div>
         </motion.div>
-
-      
-      </div>
       </div>
 
-     <Container>
-      <div className=" bg-gradient-to-r rounded-lg from-blue-500 p-2 via-teal-400 to-indigo-600  animate-gradient-x">
-     <section className="bg-white p-8 rounded-lg  text-neutral-800">
-                        <h2 className="text-3xl font-light mb-6 text-center">Skills spotlight</h2>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                            <div className="text-center">
-
-                                <h4 className="text-2xl font-bold">Unreal Engine & Maya Proficiency</h4>
-                                <p>Nearly 2 years of experience creating high-quality 3D assets and animations.</p>
-                            </div>
-                            <div className="text-center">
-
-                                <h4 className="text-2xl font-bold">VFX Professional Experience</h4>
-                                <p>Worked at Matrix VFX and Spectra VFX, delivering cutting-edge visuals.</p>
-                            </div>
-                            <div className="text-center">
-                                <h4 className="text-2xl font-bold">Expert in 3D & Visual Effects</h4>
-                                <p>Strong skills in 3D modeling, texturing, lighting, rendering, layouting and compositing.</p>
-                            </div>
-                        </div>
-                    </section>
-                    </div>
-     </Container>
+      <Container>
+        <div className=" bg-gradient-to-r rounded-lg from-blue-500 p-2 via-teal-400 to-indigo-600  animate-gradient-x">
+          <section className="bg-white p-8 rounded-lg  text-neutral-800">
+            <h2 className="text-3xl font-light mb-6 text-center">
+              Skills spotlight
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <div className="text-center">
+                <h4 className="text-2xl font-bold">
+                  Unreal Engine & Maya Proficiency
+                </h4>
+                <p>
+                  Nearly 2 years of experience creating high-quality 3D assets
+                  and animations.
+                </p>
+              </div>
+              <div className="text-center">
+                <h4 className="text-2xl font-bold">
+                  VFX Professional Experience
+                </h4>
+                <p>
+                  Worked at Matrix VFX and Spectra VFX, delivering cutting-edge
+                  visuals.
+                </p>
+              </div>
+              <div className="text-center">
+                <h4 className="text-2xl font-bold">
+                  Expert in 3D & Visual Effects
+                </h4>
+                <p>
+                  Strong skills in 3D modeling, texturing, lighting, rendering,
+                  layouting and compositing.
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </Container>
       {/* </div> */}
     </Layout>
   );
 }
 
-
 const IframeWrapper = styled.div`
-position: relative;
-    padding-bottom: 56.25%; /* 16:9 aspect ratio */
-    height: 0;
-    overflow: hidden;
-    max-width: 100%;
-    background: #000;
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 aspect ratio */
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
+  background: #000;
 
-    iframe {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      border: 0;
-    }
-
-`
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
+`;
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -10,23 +10,53 @@ interface DataItem {
 }
 
 const data: DataItem[] = [
-    {
-      "demo_reel": [
-        "https://www.youtube.com/embed/KG01kavjO9I?si=CvVD8PqbQXlspt5Z",
-        "https://www.youtube.com/embed/oMhbEONjGR8?si=Nf5WYMev6YX8071T",
-        "https://www.youtube.com/embed/gIBOr6Bxn3Q?si=JHYV8jzGl6dzL1FB",
-        "https://www.youtube.com/embed/dsnyvg0WzWc?si=hOEaCRFc1EvAgPL0",
-        "https://www.youtube.com/embed/MhcQlvBW_7s?si=MNkKOniqU5iJzWR1",
-        "https://www.youtube.com/embed/l_9t1ivExho?si=P9iH7X3mQjdn2M_i",
-      ],
-    },
-    { "animation": ["https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0"] },
-    { "short_films": ["https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0"] },
-    { "modeling": ["https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0"] },
-    { "texturing": ["https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0"] },
-    { "unreal_engine": ["https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0"] },
-  ];
-
+  {
+    Short_Films: [
+      "https://www.youtube.com/embed/KG01kavjO9I?si=CvVD8PqbQXlspt5Z",
+      "https://www.youtube.com/embed/oMhbEONjGR8?si=Nf5WYMev6YX8071T",
+      "https://www.youtube.com/embed/gIBOr6Bxn3Q?si=JHYV8jzGl6dzL1FB",
+      "https://www.youtube.com/embed/dsnyvg0WzWc?si=hOEaCRFc1EvAgPL0",
+      "https://www.youtube.com/embed/MhcQlvBW_7s?si=MNkKOniqU5iJzWR1",
+      "https://www.youtube.com/embed/l_9t1ivExho?si=P9iH7X3mQjdn2M_i",
+    ],
+  },
+  {
+    animation: [
+      "https://youtu.be/762Lzi9MXHE",
+      "https://youtu.be/_W-7NwT6Xfg",
+      "https://youtu.be/MFHxhONwKBs",
+      "https://youtu.be/ntifEGsf-5c",
+      "https://youtu.be/udUM08G8lIA",
+      "https://youtu.be/sRIPtA_Yr0M",
+      "https://youtu.be/sRIPtA_Yr0M",
+      "https://youtu.be/7ABfo5kQYs0",
+      "https://youtube.com/shorts/kE5ItZvMnKM",
+      "https://youtube.com/shorts/UpIVRTlydMY",
+      "https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0",
+    ],
+  },
+  {
+    Demo_Reel: [
+      "https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0",
+    ],
+  },
+  {
+    modeling: [
+      "https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0",
+    ],
+  },
+  {
+    texturing: [
+      "https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0",
+    ],
+  },
+  {
+    unreal_engine: [
+      "https://player.vimeo.com/video/997955764?dnt=1&title=0&byline=0&portrait=0",
+    ],
+  },
+  { Dynamics: ["https://youtu.be/QMt5sEOaA_I"] },
+];
 interface Filter {
   name: string;
   id: string;
@@ -34,17 +64,49 @@ interface Filter {
 
 const filters: Filter[] = [
   { name: "ALL", id: "all" },
-  { name: "DEMO REEL", id: "demo_reel" },
-  { name: "MODELING", id: "modeling" },
-  { name: "TEXTURING & LIGHTING", id: "texturing" },
-  { name: "3D ANIMATION", id: "animation" },
-  { name: "UNREAL ENGINE", id: "unreal_engine" },
-  { name: "SHORT FILMS", id: "short_films" },
+  ...Object.keys(data.reduce((acc, item) => ({ ...acc, ...item }), {})).map(
+    (key) => ({
+      name: key.toUpperCase().replace(/_/g, " "),
+      id: key.toLowerCase(),
+    })
+  ),
 ];
 
 const Work = () => {
   const [activeTab, setActiveTab] = useState("all");
+  // Function to convert YouTube links to embed format
+  const getYouTubeEmbedUrl = (url) => {
+    let videoId;
 
+    if (url.includes("youtube.com/embed")) {
+      return url;
+    } else if (url.includes("youtube.com/shorts")) {
+      videoId = url.split("shorts/")[1].split("?")[0];
+    } else if (url.includes("youtu.be/")) {
+      videoId = url.split("youtu.be/")[1].split("?")[0];
+    } else if (url.includes("youtube.com/watch")) {
+      const urlParams = new URLSearchParams(url.split("?")[1]);
+      videoId = urlParams.get("v");
+    }
+
+    return videoId ? `https://www.youtube.com/embed/${videoId}` : url;
+  };
+
+  const getEmbedUrl = (url) => {
+    if (!url) return "";
+
+    // Return Vimeo URLs as-is since they're already in embed format
+    if (url.includes("player.vimeo.com")) {
+      return url;
+    }
+
+    // Convert YouTube URLs to embed format
+    if (url.includes("youtube.com") || url.includes("youtu.be")) {
+      return getYouTubeEmbedUrl(url);
+    }
+
+    return url;
+  };
   return (
     <Layout>
       <HeadWithMetas
@@ -61,7 +123,9 @@ const Work = () => {
               <div
                 key={index}
                 className={cn(
-                  activeTab === filter.id ? "bg-blue-600 text-neutral-100" : "text-blue-600",
+                  activeTab === filter.id
+                    ? "bg-blue-600 text-neutral-100"
+                    : "text-blue-600",
                   "border-2 border-blue-600 cursor-pointer inline-block py-1 px-3 md:px-8 rounded-full shadow-md hover:bg-blue-700 hover:text-neutral-100 transition-colors",
                   "text-nowrap"
                 )}
@@ -74,16 +138,26 @@ const Work = () => {
           <div className="pointer-events-none absolute -top-2 bottom-0 lg:hidden right-0 w-8 bg-gradient-to-l from-neutral-600/10"></div>
         </div>
         <div className="grid grid-cols-12 md:gap-6">
-          {data.map((item, itemIndex) =>
-            Object.entries(item).map(([key, urls]) =>
-              (activeTab === "all" || activeTab === key) &&
-              urls.map((url, urlIndex) => (
-                <div key={`${itemIndex}-${urlIndex}`} className="aspect-video my-4 col-span-12 sm:col-span-6 lg:col-span-4">
-                  <iframe allowFullScreen src={url} className="w-full h-full rounded-lg shadow-sm"></iframe>
-                </div>
-              ))
-            )
-          )}
+          {Array.from(
+            data.reduce((acc, item) => {
+              const [key, urls] = Object.entries(item)[0];
+              if (activeTab === "all" || activeTab === key.toLowerCase()) {
+                urls.forEach((url) => acc.add(url)); // Add unique URLs to the set
+              }
+              return acc;
+            }, new Set())
+          ).map((url, urlIndex) => (
+            <div
+              key={urlIndex}
+              className="aspect-video my-4 col-span-12 sm:col-span-6 lg:col-span-4"
+            >
+              <iframe
+                allowFullScreen
+                src={getEmbedUrl(url)}
+                className="w-full h-full rounded-lg shadow-sm"
+              ></iframe>
+            </div>
+          ))}
         </div>
       </Container>
     </Layout>
